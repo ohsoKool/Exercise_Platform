f02718283e4f55e00a5888eeb8ffca5cc3095da4
ohsoKool <rishikeshgunda9@gmail.com>
2025-05-26 17:51:14 +0530

    Delete src directory

--- src/routes/logoutRouter.js
+++
@@ -1,8 +0,0 @@
-import { Router } from "express";
-
-export const logoutRouter = Router();
-logoutRouter.post("/logout", (req, res) => {
-  res.clearCookie("accessToken");
-  res.clearCookie("refreshToken");
-  res.json({ message: "User logged out successfully!" });
-});
