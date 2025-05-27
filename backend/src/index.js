5c65038c9b7000a6a1f5e7d48dcd34088ee2799a
ohsoKool <rishikeshgunda9@gmail.com>
2025-05-27 20:55:39 +0530

    Add saveUserProgress and getUserProgress

--- backend/src/index.js
+++ backend/src/index.js
@@ -7,6 +7,10 @@ import { tokenVerification } from "./utils/tokenHandler.js";
 import { logoutRouter } from "./routes/logoutRouter.js";
 import { exerciseRouter } from "./routes/exerciseRouter.js";
 import cors from "cors";
+import {
+  getUserProgress,
+  saveOrCompleteExercise,
+} from "./controllers/progressController.js";
 
 dotenv.config();
 const app = express();
@@ -20,16 +24,17 @@ app.use(express.json());
 app.use(cookieParser());
 
 app.use(express.static("public"));
+app.get("/", tokenVerification, (req, res) => {
+  res.send("Welcome to the / route!");
+});
 
 app.listen(process.env.PORT, () => {
   console.log(`Server is running on port ${process.env.PORT}`);
 });
 
-app.get("/", tokenVerification, (req, res) => {
-  res.send("Hello World!");
-});
-
 app.use("/api/auth", authRouter);
 app.use("/api/exercises", exerciseRouter);
 app.use("/api/token", tokenRouter);
+app.get("/api/user/getprogress", tokenVerification, getUserProgress);
+app.post("/api/user/progress/save", tokenVerification, saveOrCompleteExercise);
 app.use("/api/auth", logoutRouter);
